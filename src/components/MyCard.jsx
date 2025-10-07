aea0b63ab2a46f700b91b177662f8b8e52fdc262
boli225 <fintaboli2257@gmail.com>
2025-10-07 11:25:48 +0200

    Rating done

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -6,16 +6,17 @@ export const MyCard = ({backdrop_path,original_title
 ,release_date,vote_average}) => {
     
   return (
-    <Card sx={{ width: 345,borderRadius:"10px" }}>
+    <Card sx={{ width: 345,borderRadius:"10px", position:"relative"}}>
       <CardMedia
-        sx={{ height: 140 , borderRadius:"10px 10px 0px 0px", position:"relative"}}
+        sx={{ height: 140 , borderRadius:"10px 10px 0px 0px"}}
         image={img_500+backdrop_path}
         title={original_title}
       />
-      <CardContent>
-        <div className='rateing'>
+      <div className='rating'>
           {Math.round(vote_average*10)/10}
         </div>
+      <CardContent>
+        
         <Typography gutterBottom variant="h5" component="div">
           {original_title}
         </Typography>
